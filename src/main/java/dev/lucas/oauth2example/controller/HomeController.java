bedd0511ab9778aebb7bc896cc8ab26ffc1be114
lucasruviaro <lucasruviaro@hotmail.com>
2023-06-14 09:15:32 -0300

    final commit

---
+++ src/main/java/dev/lucas/oauth2example/controller/HomeController.java
@@ -0,0 +1,20 @@
+package dev.lucas.oauth2example.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Home!";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, Secured!";
+    }
+
+}
